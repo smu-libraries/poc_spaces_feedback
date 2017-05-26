3617c94b9c8da364ef33afac9097ee44ef3fc644
Ler Wee Hiong <hum.ler@outlook.com>
2017-05-27 01:43:21 +0800

    Check Origin in the headers

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
  * This is a quick hack for the feedback service for the Spaces project. Spaces submits feedback using an AMP Form, which requires the service to implement the CORS specification. This code does not even stop for errors, or check the origin, or perform input validation, or do persistence.
  */
 
+let EXPECTED_ORIGIN = 'https://lti.library.smu.edu.sg';
+
 let express = require('express');
 let cors = require('cors');
 let multer = require('multer');
@@ -52,13 +54,17 @@ app.get('/poc_spaces_feedback', (req, res, next) => {
 });
 
 app.post('/poc_spaces_feedback', multer().single(), cors(cors_options), function (req, res, next) {
-  feedback.create({
-    app_id: req.body.app_id,
-    rating: req.body.rating
-  }).then(() => {
-    res.set('AMP-Access-Control-Allow-Source-Origin', req.query.__amp_source_origin);
-    res.json({});
-  });
+  if (!req.headers.Origin || !req.query.__amp_source_origin || req.headers.Origin !== EXPECTED_ORIGIN || req.headers.Origin !== req.query.__amp_source_origin) {
+    res.status(403).end();
+  } else {
+    feedback.create({
+      app_id: req.body.app_id,
+      rating: req.body.rating
+    }).then(() => {
+      res.set('AMP-Access-Control-Allow-Source-Origin', req.query.__amp_source_origin);
+      res.json({});
+    });
+  }
 });
 
 app.listen(process.env.PORT || 5000);
